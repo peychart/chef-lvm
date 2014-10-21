d1de3774ed90fec1bb107cb4e8e6df91fa95e16a
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2014-10-21 11:15:03 -1000

    remove dependencie to chef-nodeAttributes

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,5 @@ description      'Installs/Configures chef-lvm'
 long_description 'Installs/Configures chef-lvm'
 version          '0.1.0'
 
-depends          'chef-nodeAttributes'
 depends          'lvm'
 
